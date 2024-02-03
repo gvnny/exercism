93262925ad0f26428ece561cc57001c70666602d
Geovanny N Silva <geovanny.gns1@gmail.com>
2024-02-03 11:13:14 -0300

    javascript darts #3

--- javascript/darts/darts.js
+++ javascript/darts/darts.js
@@ -1,16 +1,14 @@
 export const score = (x, y) => {
-  const distance = Math.sqrt((x) ** 2 + (y) ** 2);
+  const distance = Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
 
   if (distance > 10) {
     return 0;
   }
-  if (distance <= 10 && distance > 5) {
+  if (distance > 5) {
     return 1;
-  } 
-  if (distance <= 5 && distance > 1) {
+  }
+  if (distance > 1) {
     return 5;
   }
-  if (distance <= 1) {
-    return 10;
-  } 
+  return 10;
 };
