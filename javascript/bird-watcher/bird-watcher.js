95e68096f51d61fec02ee6ff1f13747be0d2775c
Geovanny N Silva <geovanny.gns1@gmail.com>
2024-02-06 17:27:13 -0300

    javascript bird-watcher #1

---
+++ javascript/bird-watcher/bird-watcher.js
@@ -0,0 +1,38 @@
+/**
+ * Calculates the total bird count.
+ *
+ * @param {number[]} birdsPerDay
+ * @returns {number} total bird count
+ */
+export function totalBirdCount(birdsPerDay) {
+  return birdsPerDay.reduce((accumulator, element) => accumulator + element, 0);
+}
+
+/**
+ * Calculates the total number of birds seen in a specific week.
+ *
+ * @param {number[]} birdsPerDay
+ * @param {number} week
+ * @returns {number} birds counted in the given week
+ */
+export function birdsInWeek(birdsPerDay, week) {
+  const initial = (7 * week) - 7;
+  const arrayWeek = birdsPerDay.splice(initial, 7);
+
+  return totalBirdCount(arrayWeek);
+}
+
+/**
+ * Fixes the counting mistake by increasing the bird count
+ * by one for every second day.
+ *
+ * @param {number[]} birdsPerDay
+ * @returns {number[]} corrected bird count data
+ */
+export function fixBirdCountLog(birdsPerDay) {
+  for (let i = 0; i < birdsPerDay.length ; i += 2)  {
+    birdsPerDay[i] += 1;
+  }
+  
+  return birdsPerDay;
+}
