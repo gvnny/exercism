762978c1f4259d74d653282110948e64744347f2
Geovanny N Silva <geovanny.gns1@gmail.com>
2024-02-07 19:10:28 -0300

    javascript bird-watcher #2

--- javascript/bird-watcher/bird-watcher.js
+++ javascript/bird-watcher/bird-watcher.js
@@ -30,7 +30,7 @@ export function birdsInWeek(birdsPerDay, week) {
  * @returns {number[]} corrected bird count data
  */
 export function fixBirdCountLog(birdsPerDay) {
-  for (let i = 0; i < birdsPerDay.length ; i += 2)  {
+  for (let i = 0 ; i < birdsPerDay.length ; i += 2)  {
     birdsPerDay[i] += 1;
   }
   
