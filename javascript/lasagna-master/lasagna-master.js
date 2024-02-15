afa991a8d6257aa7c7955c023a4f296919df8ad6
Geovanny N Silva <geovanny.gns1@gmail.com>
2024-02-15 18:37:51 -0300

    javascript lasagna-master #1

---
+++ javascript/lasagna-master/lasagna-master.js
@@ -0,0 +1,58 @@
+/**
+ * Implement the functions needed to solve the exercise here.
+ * Do not forget to export them so they are available for the
+ * tests. Here an example of the syntax as reminder:
+ *
+ * export function yourFunction(...) {
+ *   ...
+ * }
+ */
+
+export function cookingStatus(timeLeft) {
+    if (timeLeft > 0) {
+        return 'Not done, please wait.';
+    }
+    if (timeLeft === 0) {
+        return 'Lasagna is done.';
+    }
+
+    return 'You forgot to set the timer.';
+}
+
+export function preparationTime(layers, avarageTime) {
+    if (!avarageTime) {
+        avarageTime = 2;
+    }
+
+    return layers.length * avarageTime;
+}
+
+export function quantities(layers) {
+    let sauce = 0;
+    let noodles = 0;
+
+    for (let i = 0; i <= layers.length; i++) {
+        if (layers[i] === 'sauce') {
+            sauce += 1;
+        }
+        if (layers[i] === 'noodles') {
+            noodles += 1;
+        }
+    }
+
+    return { noodles: noodles * 50, sauce: sauce * 0.2 };
+}
+
+export function addSecretIngredient(friendList, yourList) {
+    yourList.push(friendList.reduce((acc, cur) => cur));
+}
+
+export function scaleRecipe(recipe, portions) {
+    const proportionalIncome = {};
+
+    for (let i in recipe) {
+        proportionalIncome[i] = recipe[i] * (portions / 2);
+    }
+
+    return proportionalIncome;
+}
