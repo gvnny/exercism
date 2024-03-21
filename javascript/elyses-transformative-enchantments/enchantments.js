40feee50b9340518db428597f91646fb606ea5f7
Geovanny N Silva <geovanny.gns1@gmail.com>
2024-03-21 16:01:03 -0300

    javascript elyses-transformative-enchantments #1

---
+++ javascript/elyses-transformative-enchantments/enchantments.js
@@ -0,0 +1,105 @@
+// @ts-check
+
+/**
+ * Double every card in the deck.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with every card doubled
+ */
+export function seeingDouble(deck) {
+  return deck.map((value) => value * 2);
+}
+
+/**
+ *  Creates triplicates of every 3 found in the deck.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with triplicate 3s
+ */
+export function threeOfEachThree(deck) {
+  const newDeck = [];
+
+  for (const card of deck) {
+    newDeck.push(card);
+    if (card === 3) {
+      newDeck.push(3, 3);
+    }
+  }
+
+  return newDeck;
+}
+
+/**
+ * Extracts the middle two cards from a deck.
+ * Assumes a deck is always 10 cards.
+ *
+ * @param {number[]} deck of 10 cards
+ *
+ * @returns {number[]} deck with only two middle cards
+ */
+export function middleTwo(deck) {
+  const half = deck.length / 2;
+
+  return deck.slice((half - 1), (half + 1));
+}
+
+/**
+ * Moves the outside two cards to the middle.
+ *
+ * @param {number[]} deck with even number of cards
+ *
+ * @returns {number[]} transformed deck
+ */
+
+export function sandwichTrick(deck) {
+  const newDeck = deck.slice();
+  const half = deck.length / 2;
+
+  newDeck.splice(half, 0, deck[0]);
+  newDeck.splice(half, 0, deck[deck.length - 1]);
+  newDeck.pop();
+  newDeck.shift();
+
+  return newDeck;
+}
+
+/**
+ * Removes every card from the deck except 2s.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with only 2s
+ */
+export function twoIsSpecial(deck) {
+  return deck.filter((value) => value === 2);
+}
+
+/**
+ * Returns a perfectly order deck from lowest to highest.
+ *
+ * @param {number[]} deck shuffled deck
+ *
+ * @returns {number[]} ordered deck
+ */
+export function perfectlyOrdered(deck) {
+  return deck.sort((element1, element2) => {
+    if (element1 < element2) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+}
+
+/**
+ * Reorders the deck so that the top card ends up at the bottom.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} reordered deck
+ */
+export function reorder(deck) {
+  return deck.reverse();
+}
